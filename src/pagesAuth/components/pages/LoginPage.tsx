bf9f08cdcf33f12444a4b9e608538c96ee518aac
Elcho911 <boss.armsport@gmail.com>
2024-06-22 01:07:06 +0600

    update validation

--- src/pagesAuth/components/pages/LoginPage.tsx
+++ src/pagesAuth/components/pages/LoginPage.tsx
@@ -11,9 +11,78 @@ interface IFormInput {
 	password: string;
 }
 
+const InputField = ({ name, control, rules, placeholder, errors }: any) => (
+	<Controller
+		name={name}
+		control={control}
+		rules={rules}
+		render={({ field }) => (
+			<Input
+				status={errors[name] ? 'error' : ''}
+				className={scss.input}
+				size="large"
+				placeholder={placeholder}
+				{...field}
+			/>
+		)}
+	/>
+);
+
+const PasswordField = ({ name, control, rules, placeholder, errors }: any) => (
+	<Controller
+		name={name}
+		control={control}
+		rules={rules}
+		render={({ field }) => (
+			<Input.Password
+				status={errors[name] ? 'error' : ''}
+				className={scss.input}
+				size="large"
+				placeholder={placeholder}
+				{...field}
+			/>
+		)}
+	/>
+);
+
+const LoginForm = ({
+	handleSubmit,
+	control,
+	errors,
+	handleRememberMeChange,
+	onSubmit
+}: any) => (
+	<form onSubmit={handleSubmit(onSubmit)}>
+		<InputField
+			name="email"
+			control={control}
+			rules={{ required: true, minLength: 2 }}
+			placeholder="Номер телефона, имя пользователя или email"
+			errors={errors}
+		/>
+		<PasswordField
+			name="password"
+			control={control}
+			rules={{ required: true, minLength: 2 }}
+			placeholder="Пароль"
+			errors={errors}
+		/>
+		<Checkbox className={scss.customCheckbox} onChange={handleRememberMeChange}>
+			Сохранить вход
+		</Checkbox>
+		<Button type="primary" size="large" block htmlType="submit">
+			Войти
+		</Button>
+	</form>
+);
+
 const LoginPage = () => {
 	const [postLoginMutation] = usePostLoginMutation();
-	const { control, handleSubmit } = useForm<IFormInput>();
+	const {
+		control,
+		handleSubmit,
+		formState: { errors }
+	} = useForm<IFormInput>();
 	const [rememberMe, setRememberMe] = useState(false);
 
 	const handleRememberMeChange = (e: CheckboxChangeEvent) => {
@@ -33,52 +102,18 @@ const LoginPage = () => {
 		}
 	};
 
-	const renderForm = () => (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<Controller
-				name="email"
-				control={control}
-				rules={{ required: true, minLength: 2 }}
-				render={({ field }) => (
-					<Input
-						className={scss.input}
-						size="large"
-						placeholder="Номер телефона, имя пользователя или email"
-						{...field}
-					/>
-				)}
-			/>
-			<Controller
-				name="password"
-				control={control}
-				rules={{ required: true, minLength: 2 }}
-				render={({ field }) => (
-					<Input.Password
-						className={scss.input}
-						size="large"
-						placeholder="Пароль"
-						{...field}
-					/>
-				)}
-			/>
-			<Checkbox
-				className={scss.customCheckbox}
-				onChange={handleRememberMeChange}
-			>
-				Сохранить вход
-			</Checkbox>
-			<Button type="primary" size="large" block htmlType="submit">
-				Войти
-			</Button>
-		</form>
-	);
-
 	return (
 		<section className={scss.LoginPage}>
 			<div className={scss.container}>
 				<div className={scss.content}>
 					<img className={scss.logo} src={logo} alt="logo" />
-					{renderForm()}
+					<LoginForm
+						handleSubmit={handleSubmit}
+						control={control}
+						errors={errors}
+						handleRememberMeChange={handleRememberMeChange}
+						onSubmit={onSubmit}
+					/>
 				</div>
 			</div>
 		</section>
